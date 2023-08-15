d438d928633f2f3aa1dacebe366748d10342e262
Roma <elusiuve15.09.93@icloud.com>
2023-08-15 18:05:01 +0300

    General settings

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
 import './App.css';
+import c from './App/Container.module.scss'
+import w from './App/Wrapper.module.scss'
+import m from './App/Main.module.scss'
+import { Footer } from './components/Footer/Footer';
+import { Header } from './components/Header/Header';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        Loading...
-      </header>
-    </div>
+      <div className={w.wrapper}>
+        <Header />
+        <main className={m.main}>
+          <div className={c.container}>
+            Container
+          </div>
+        </main>
+        <Footer />
+      </div>
+    </div >
   );
 }
 
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,9 @@
+import style from "./Header.module.scss"
+
+export const Header = () => {
+    return (
+        <div className={style.header}>
+            <div>Header</div>
+        </div>
+    )
+}
